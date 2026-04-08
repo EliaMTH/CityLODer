b1df9d0453ddfeb70a58eaf93db8fc5382d4dd65
Tommaso Sorgente <tommaso.sorgente@ge.imati.cnr.it>
2026-04-08 15:06:24 +0200

    new version of CPP1

--- CPP1/src/auxiliary.h
+++ CPP1/src/auxiliary.h
@@ -4,8 +4,12 @@
 #include <cinolib/meshes/meshes.h>
 
 #include <filesystem>
+#include <iostream>
+#include <streambuf>
 
-using namespace cinolib;
+namespace cinolib
+{
+
 namespace fs = std::filesystem;
 
 /*************** GEOMETRIC OPERATIONS ****************/
@@ -21,7 +25,7 @@ void map_z_vals(const AbstractPolygonMesh<M,V,E,P> &m, std::unordered_map<uint, 
 }
 
 template<class M, class V, class E, class P>
-void project(AbstractPolygonMesh<M,V,E,P> &m, std::unordered_map<uint, double> &list)
+void project_mesh(AbstractPolygonMesh<M,V,E,P> &m, std::unordered_map<uint, double> &list)
 {
     for (uint vid=0; vid<m.num_verts(); ++vid) {
         if (list.find(vid) == list.end()) {
@@ -33,27 +37,43 @@ void project(AbstractPolygonMesh<M,V,E,P> &m, std::unordered_map<uint, double> &
 }
 
 template<class M, class V, class E, class P>
-void project_back(AbstractPolygonMesh<M,V,E,P> &m, const std::unordered_map<uint, double> &list)
+void project_back_mesh(AbstractPolygonMesh<M,V,E,P> &m, const std::unordered_map<uint, double> &list)
 {
     int counter = 0;
+    std::vector<uint> new_vids;
     for (uint vid=0; vid<m.num_verts(); ++vid) {
         auto it = list.find(vid);
         if (it != list.end()) {
             m.vert(vid).z() = it->second;
         } else {
+            new_vids.push_back(vid);
             ++counter;
             // std::cerr << "project_back - ERROR: the vertex does not exist" << std::endl;
             // assert(false);
         }
     }
+    // set the z-coordinate of the new vertices to the average z-coordinate of their neighbors (excluding other new vertices)
+    for (uint vid : new_vids) {
+        double z = 0.;
+        int n_neigh = 0;
+        for (uint nbr : m.adj_v2v(vid)) {
+            if (std::find(new_vids.begin(), new_vids.end(), nbr) == new_vids.end()) {
+                z += m.vert(nbr).z();
+                ++n_neigh;
+            }
+        }
+        if (n_neigh > 0) {
+            m.vert(vid).z() = z / n_neigh;
+        }
+    }
     m.update_bbox();
     if (counter > 0) {
         std::cerr << "  project_back - WARNING: " << counter
-                  << " vertices were not found in the z_map" << std::endl;
+                  << " vertices were not found in the z_map, z-coordinate computed manually" << std::endl;
     }
 }
 
-void project(const uint id, vec3d &p, std::unordered_map<uint, double> &list)
+void project_point(const uint id, vec3d &p, std::unordered_map<uint, double> &list)
 {
     if (list.find(id) == list.end()) {
         list[id] = p.z();
@@ -61,7 +81,7 @@ void project(const uint id, vec3d &p, std::unordered_map<uint, double> &list)
     p.z() = 0.;
 }
 
-void project_back(const uint id, vec3d &p, const std::unordered_map<uint, double> &list)
+void project_back_point(const uint id, vec3d &p, const std::unordered_map<uint, double> &list)
 {
     auto it = list.find(id);
     if (it != list.end()) {
@@ -95,6 +115,45 @@ void append_map(const std::vector<vec3d> &verts1,
     }
 }
 
+bool mesh_has_duplicates(const Polygonmesh<> &m)
+{
+    std::vector<vec3d> verts = m.vector_verts();
+    REMOVE_DUPLICATES_FROM_VEC(verts);
+    return (verts.size() != m.num_verts());
+}
+
+template<class M, class V, class E, class P>
+bool is_mesh_inside(const AbstractPolygonMesh<M,V,E,P> &m0, const AbstractPolygonMesh<M,V,E,P> &m1)
+{
+    // check if the mesh m1 is completely inside the mesh m0
+    assert(m0.num_polys() == 1 && "WARNING - is_mesh_inside: m0 has more than one poly");
+    for(uint pid=0; pid<m1.num_polys(); ++pid) {
+        for (vec3d &v : m1.poly_verts(pid)) {
+            if (!point_in_polygon(m0, v, 0)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+template<class M, class V, class E, class P>
+bool polygons_intersect(const AbstractPolygonMesh<M,V,E,P> &m, const uint pid0, const uint pid1)
+{
+    assert(m.num_polys() > std::max(pid0, pid1));
+    for (uint eid0 : m.adj_p2e(pid0)) {
+        for (uint eid1 : m.adj_p2e(pid1)) {
+            std::vector<vec3d> verts0 = m.edge_verts(eid0);
+            std::vector<vec3d> verts1 = m.edge_verts(eid1);
+            if (segment_segment_intersect_2d(verts0[0].rem_coord(), verts0[1].rem_coord(),
+                                             verts1[0].rem_coord(), verts1[1].rem_coord())
+                != DO_NOT_INTERSECT)
+                return true;
+        }
+    }
+    return false;
+}
+
 template<class M, class V, class E, class P>
 bool point_in_polygon(const AbstractPolygonMesh<M,V,E,P> &m,
                       const vec3d p,
@@ -122,13 +181,18 @@ bool point_in_polygon(const AbstractPolygonMesh<M,V,E,P> &m,
 }
 
 template<class M, class V, class E, class P>
-vec3d pick_point_in_polygon(const AbstractPolygonMesh<M,V,E,P> &m,
+vec3d pick_point_in_polygon(AbstractPolygonMesh<M,V,E,P> &m,
                             const uint pid)
 {
+    m.update_p_tessellation(pid);
     std::vector<uint> tess = m.poly_tessellation(pid);
+    if (tess.empty()) {
+        std::cerr << "pick_point_in_polygon - ERROR: could not triangulate polygon " << pid << std::endl;
+        assert(false);
+    }
     vec3d c;
-    double eps = .1;
-    double shift = .1;
+    double eps   = 0.1;
+    double shift = 0.1;
     for (uint i=0; i<tess.size()-2; i=i+3) {
         vec3d v0 = m.vert(tess.at(i));
         vec3d v1 = m.vert(tess.at(i+1));
@@ -147,8 +211,23 @@ vec3d pick_point_in_polygon(const AbstractPolygonMesh<M,V,E,P> &m,
             return c;
         }
     }
-    std::cerr << "pick_point_in_polygon - ERROR: could not find a point inside the polygon " << pid << std::endl;
+    std::cerr << "pick_point_in_polygon - ERROR: could not find a point inside polygon " << pid << std::endl;
     return c;
+}
+
+int closest_point(const std::vector<vec2d> &points,
+                   const vec2d &p)
+{
+    double MAX_DIST = DBL_MAX;
+    int closest = -1;
+    for (uint vid=0; vid<points.size(); ++vid) {
+        double d = points.at(vid).dist_sqrd(p);
+        if (d < MAX_DIST) {
+            closest = vid;
+            MAX_DIST = d;
+        }
+    }
+    return closest;
 }
 
 /*************** IO OPERATIONS ****************/
@@ -185,41 +264,54 @@ int extract_directory_ID(const std::string& directory)
     return std::stoi(numberStr);
 }
 
-// print a csv file with the ID of each poly and its label
 template<class M, class V, class E, class P>
-void print_poly_labels(const AbstractPolygonMesh<M,V,E,P> &m,
-                       const std::string &file_path)
+DrawablePolygonmesh<> convert_to_drawable(const AbstractPolygonMesh<M,V,E,P> &m)
 {
-    std::ofstream file(file_path);
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file.\n";
-        return;
+    DrawablePolygonmesh<> dm(m.vector_verts(), m.vector_polys());
+
+    for (uint pid=0; pid<dm.num_polys(); ++pid) {
+        dm.poly_data(pid).label = m.poly_data(pid).label;
     }
 
-    file << "poly_ID building_ID\n";
-    for (uint pid=0; pid<m.num_polys(); ++pid) {
-        file << pid << " " << m.poly_data(pid).label << "\n";
+    for (uint eid=0; eid<dm.num_edges(); ++eid) {
+        dm.edge_data(eid).label = m.edge_data(eid).label;
+        dm.edge_data(eid).flags[MARKED] = (dm.edge_data(eid).label != -1);
     }
-    file.close();
+
+    for (uint vid=0; vid<dm.num_verts(); ++vid) {
+        dm.vert_data(vid).label = m.vert_data(vid).label;
+        dm.vert_data(vid).flags[MARKED] = (dm.vert_data(vid).label != -1);
+        if (dm.vert_data(vid).label != -1) {
+            dm.vert_data(vid).color = Color::BLUE();
+            // gui.push_marker(dm.vert(vid), "", Color::BLUE(), 2);
+        }
+    }
+
+    return dm;
 }
 
-// print a csv file with the ID of each poly and its label
-template<class M, class V, class E, class P>
-void print_edge_labels(const AbstractPolygonMesh<M,V,E,P> &m,
-                       const std::string &file_path)
+struct NullBuffer : std::streambuf
 {
-    std::ofstream file(file_path);
-    if (!file.is_open()) {
-        std::cerr << "Failed to open file.\n";
-        return;
-    }
+    int overflow(int c) override { return c; } // discard prints
+};
 
-    file << "edge_v0_ID edge_v1_ID edge_ID\n";
-    for (uint eid=0; eid<m.num_edges(); ++eid) {
-        auto verts = m.edge_vert_ids(eid);
-        file << verts.front() << " " << verts.back() << " " << m.edge_data(eid).label << "\n";
+std::streambuf* suppress_stdout()
+{
+    std::cout<<std::flush;
+    static NullBuffer null_buf;
+    std::streambuf* old_buf = std::cout.rdbuf(&null_buf); // hides cout prints
+    return old_buf;
+}
+
+void restore_stdout(std::streambuf* old_buf)
+{
+    if (old_buf) {
+        std::cout.rdbuf(old_buf); // restore cout
+        old_buf = nullptr;
     }
-    file.close();
+    std::cout<<std::flush;
+}
+
 }
 
 #endif // CITY_AUXILIARY
